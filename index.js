f984b8c63cc52f16e671781c1bd924a2179ff06b
adrianburgos <adrian.burgos.colas@gmail.com>
2021-11-07 17:37:26 -0500

    tests from 1 to 14 ok

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    
+    //AJAX
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET','https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json');
+    xhr.send();
+    xhr.onload = ()=>{
+        const dataset = JSON.parse(xhr.responseText)
+        // console.log(dataset)
+        const w = 800;
+        const h = 500;
+        const padding = 70;
+
+        const xScale = d3.scaleLinear()
+                        .domain([d3.min(dataset,(d)=>d.Year),d3.max(dataset,(d)=>d.Year)])
+                        .range([padding,w-padding]);
+
+        const yScale = d3.scaleLinear()
+                        .domain([d3.min(dataset,(d)=>d.Seconds*1000),d3.max(dataset,(d)=>d.Seconds*1000)])
+                        .range([h-padding,padding]);
+
+        const chart = d3.select('main')
+                        .select('div')
+                        .append('svg')
+                        .attr('width',w)
+                        .attr('height',h);
+
+        chart.selectAll('circle')
+        .data(dataset)
+        .enter()
+        .append('circle')
+        .attr('class','dot')
+        .attr('data-xvalue',(d)=>d.Year)
+        .attr('data-yvalue',(d)=>{
+            const date = new Date(d.Seconds*1000)
+            return date
+        })
+        .attr('cx',(d)=>xScale(d.Year))
+        .attr('cy',(d)=>yScale(d.Seconds*1000))
+        .attr('r',5)
+        
+        
+        const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('Y'));
+        chart.append('g')
+            .attr('id','x-axis')
+            .attr('transform',`translate(0,${h-padding})`)
+            .call(xAxis);
+
+        const yAxis = d3.axisLeft(yScale).tickFormat((d)=>{return d3.timeFormat('%M:%S')(d)});
+        chart.append('g')
+            .attr('id','y-axis')
+            .attr('transform',`translate(${padding},0)`)
+            .call(yAxis);
+
+        chart.append('g').attr('id','legend').text('legend')
+        }
+        
 });
